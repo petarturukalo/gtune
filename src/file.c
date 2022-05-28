cc079384a96da8221f96f0ce43d832338e73897d
pt <petarturukalo@gmail.com>
2022-05-28 15:04:14 +1000

    rm unused func

--- src/file.c
+++ src/file.c
@@ -14,22 +14,3 @@ int filesz(int fd)
 	return s.st_size;
 }
 
-short *fread_sint16(char *fpath, int *n)
-{
-	short *buf;
-	int fsz, fd;
-
-	fd = open(fpath, O_RDONLY);
-	fsz = filesz(fd);
-
-	// File is in bytes, and each buffer element is 2 bytes.
-	// Assumes the file size is even.
-	*n = fsz/2;  
-	buf = malloc(*n*sizeof(short));
-	read(fd, (void *)buf, fsz);
-
-	close(fd);
-
-	return buf;
-}
-
--- src/file.h
+++ src/file.h
@@ -20,13 +20,4 @@
  */
 int filesz(int fd);
 
-/*
- * fread_sint16 - Open and read the contents of a file interpreting it as signed 16-bit integers
- * @fpath: path to file
- * @n: out-param where to store length of allocated buffer
- *
- * Returned buffer is dynamically allocated and must be freed with free.
- */
-short *fread_sint16(char *fpath, int *n);
-
 #endif
