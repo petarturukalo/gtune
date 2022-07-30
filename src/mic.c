4e6ee9f755202214043aa736d305343b4129a395
pt <petarturukalo@gmail.com>
2022-07-30 22:06:44 +1000

    - let user input pulse audio sample data type and size - update mic_read() to handle errors

--- src/mic.c
+++ src/mic.c
@@ -10,27 +10,29 @@
  *
  * Return -1 on couldn't get the default device. Return 0 on success.
  */
-int mic_set_params(PaStreamParameters *p)
+int mic_set_params(PaStreamParameters *p, PaSampleFormat fmt)
 {
 	const PaDeviceInfo *info;
 
 	p->device = Pa_GetDefaultInputDevice();
 
-	if (p->device == paNoDevice)
+	if (p->device == paNoDevice) {
+		eprintf("couldn't get default input device");
 		return -1;
+	}
 	info = Pa_GetDeviceInfo(p->device);
 	// Not a very helpful name since it's just "default",
 	// but print it anyway.
 	printf("using audio input device '%s'\n", info->name);
 
 	p->channelCount = 1;
-	p->sampleFormat = paFloat32;
+	p->sampleFormat = fmt;
 	p->suggestedLatency = info->defaultLowInputLatency;
 	p->hostApiSpecificStreamInfo = NULL;
 	return 0;
 }
 
-bool mic_init(mic_t *m, int sample_rate, int readsz)
+bool mic_init(mic_t *m, uint sample_rate, uint readsz, PaSampleFormat fmt)
 {
 	PaError err;
 	PaStreamParameters mic_params;
@@ -38,27 +40,26 @@ bool mic_init(mic_t *m, int sample_rate, int readsz)
 	err = Pa_Initialize();
 
 	if (err != paNoError) {
-		err_print("couldn't init portaudio", Pa_GetErrorText(err));
+		eprintf("couldn't init portaudio: %s", Pa_GetErrorText(err));
 		return false;
 	}
-	err = mic_set_params(&mic_params);
+	err = mic_set_params(&mic_params, fmt);
 
 	if (err == -1) {
-		err_print("couldn't get default input device", Pa_GetErrorText(err));
 		Pa_Terminate();
 		return false;
 	}
 	err = Pa_OpenStream(&m->stream, &mic_params, NULL, sample_rate, readsz, 
 			    paClipOff, NULL, NULL);
 	if (err != paNoError) {
-		err_print("couldn't open audio input stream", Pa_GetErrorText(err));
+		eprintf("couldn't open audio input stream: %s", Pa_GetErrorText(err));
 		Pa_Terminate();
 		return false;
 	}
 	err = Pa_StartStream(m->stream);
 
 	if (err != paNoError) {
-		err_print("couldn't start audio input stream", Pa_GetErrorText(err));
+		eprintf("couldn't start audio input stream: %s", Pa_GetErrorText(err));
 		Pa_CloseStream(m->stream);
 		Pa_Terminate();
 		return false;
@@ -66,9 +67,12 @@ bool mic_init(mic_t *m, int sample_rate, int readsz)
 	return true;
 }
 
-void mic_read(mic_t *m, float *samples, int readsz)
+void mic_read_until_success(mic_t *m, char *samples, uint readsz)
 {
-	Pa_ReadStream(m->stream, (void *)samples, readsz);
+	PaError err;
+
+	while ((err = Pa_ReadStream(m->stream, (void *)samples, readsz)) != paNoError)
+		eprintf("failed to read audio input samples: %s", Pa_GetErrorText(err));
 }
 
 void mic_cleanup(mic_t *m)
--- src/mic.h
+++ src/mic.h
@@ -26,21 +26,23 @@ typedef struct microphone mic_t;
  * @sample_rate: samples per second (Hz)
  * @readsz: number of samples per read (although a read doesn't have to match this, but
  *	it should for performance reasons)
+ * @fmt: pulse audio's description of a sample's data type
  * 
  * Uses the default microphone. Return whether the intialisation was successful. Starts the underlying
  * stream which can be stopped with mic_free (which also cleans up the data structure).
  */
-bool mic_init(mic_t *m, int sample_rate, int readsz);
+bool mic_init(mic_t *m, uint sample_rate, uint readsz, PaSampleFormat fmt);
 
 /*
- * mic_read - Read samples from a microphone
+ * mic_read_until_success - Read samples from a microphone. If a read fails it keeps retrying 
+ *	until it succeeds.
  * @samples: array to store read samples into
- * @readsz: number of samples to read (preferrably the same as sthe init-time read size)
+ * @readsz: number of samples to read (preferrably the same as the init-time read size)
  *
  * Microphone must have been initialised with call to mic_init before calling this.
  * Blocks until underlying samples buffer is filled with the read size amount of samples.
  */
-void mic_read(mic_t *m, float *samples, int readsz);
+void mic_read_until_success(mic_t *m, char *samples, uint readsz);
 
 /*
  * mic_cleanup - Clean up a microphone data structure initialised with mic_init
